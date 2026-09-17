432ca9f30c3c1b2e638a80eb7646bb7b3a30894b
modisanket191 <62167187+modisanket191@users.noreply.github.com>
2026-09-17 13:30:12 +0530

    Create separate main JavaScript file

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// General portfolio JavaScript
+// Chess functionality remains isolated in assets/js/chess.js.
+
+(() => {
+  'use strict';
+
+  // General site behavior can be added here without mixing it with Chess.com logic.
+})();
